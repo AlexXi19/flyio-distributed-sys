06964cf06fe174a2e7ae3c1892ccd4fdbf93d166
AlexXi19 <alex2001314jjj@gmail.com>
2023-04-09 23:29:19 -0700

    efficient broadcasting

--- src/bin/broadcast-multinode-fault-tolerant/main.rs
+++ src/bin/broadcast-multinode-fault-tolerant/main.rs
@@ -34,7 +34,7 @@ enum Payload {
 struct BroadcastNode {
     id: usize,
     messages: HashSet<u32>,
-    neighbors: HashMap<String, Vec<String>>,
+    neighbors: HashMap<String, HashSet<u32>>,
     curr_node: String,
 }
 
@@ -44,20 +44,14 @@ impl Node<(), Payload> for BroadcastNode {
         init: Init,
         _tx: std::sync::mpsc::Sender<Event<Payload>>,
     ) -> anyhow::Result<Self> {
-        let mut other_nodes = init.node_ids.clone();
-        let index = init
-            .node_ids
-            .iter()
-            .position(|x| *x == init.node_id)
-            .unwrap();
-        other_nodes.remove(index);
-
-        Ok(BroadcastNode {
+        let broadcast_node = BroadcastNode {
             id: 1,
             messages: HashSet::new(),
             neighbors: HashMap::new(),
             curr_node: init.node_id,
-        })
+        };
+
+        Ok(broadcast_node)
     }
 
     fn step(&mut self, input: Event<Payload>, output: &mut StdoutLock) -> anyhow::Result<()> {
@@ -65,6 +59,7 @@ impl Node<(), Payload> for BroadcastNode {
             panic!("got injected event when there's no event injection");
         };
 
+        let message_src = input.src.clone();
         let mut reply = input.into_reply(Some(&mut self.id));
         match reply.body.payload {
             Payload::Broadcast { message } => {
@@ -75,17 +70,37 @@ impl Node<(), Payload> for BroadcastNode {
                 self.sync_with_neighbors(output)?
             }
             Payload::Gossip { seen } => {
+                let neighbor_vec = match self.neighbors.get_mut(&message_src) {
+                    Some(x) => x,
+                    None => return Ok(()),
+                };
+
+                for message in seen.clone() {
+                    self.messages.insert(message);
+                    neighbor_vec.insert(message);
+                }
+
+                let mut gossip_ok = HashSet::new();
+                for message in self.messages.clone().into_iter() {
+                    // Need to send the seen back for the sender to know that we have seen it
+                    if !neighbor_vec.contains(&message) || seen.contains(&message) {
+                        gossip_ok.insert(message);
+                    }
+                }
+
                 reply.body.payload = Payload::GossipOk {
-                    seen: self.messages.clone().into_iter().collect(),
+                    seen: gossip_ok.into_iter().collect(),
                 };
                 reply.send(output)?;
-                for message in seen {
-                    self.messages.insert(message);
-                }
             }
             Payload::GossipOk { seen } => {
+                let neighbor_vec = match self.neighbors.get_mut(&message_src) {
+                    Some(x) => x,
+                    None => return Ok(()),
+                };
                 for message in seen {
                     self.messages.insert(message);
+                    neighbor_vec.insert(message);
                 }
             }
             Payload::BroadcastOk { .. } => {}
@@ -102,7 +117,7 @@ impl Node<(), Payload> for BroadcastNode {
                 let empty_vec = vec![];
                 let topology = topology.get(&self.curr_node).unwrap_or(&empty_vec);
                 for node in topology {
-                    self.neighbors.insert(node.clone(), vec![]);
+                    self.neighbors.insert(node.clone(), HashSet::new());
                 }
                 reply.body.payload = Payload::TopologyOk {};
                 reply.send(output)?
@@ -117,15 +132,23 @@ impl Node<(), Payload> for BroadcastNode {
 impl BroadcastNode {
     fn sync_with_neighbors(&self, output: &mut StdoutLock) -> anyhow::Result<()> {
         for node in self.neighbors.keys().into_iter() {
+            let seen_nodes = match self.neighbors.get(node) {
+                Some(x) => x,
+                None => return Ok(()),
+            };
+            let unseen_nodes = self
+                .messages
+                .clone()
+                .into_iter()
+                .filter(|x| !seen_nodes.contains(x))
+                .collect::<Vec<_>>();
             let gossip = Message {
                 src: self.curr_node.clone(),
                 dst: node.clone(),
                 body: Body {
                     id: None,
                     in_reply_to: None,
-                    payload: Payload::Gossip {
-                        seen: self.messages.clone().into_iter().collect(),
-                    },
+                    payload: Payload::Gossip { seen: unseen_nodes },
                 },
             };
             gossip.send(output)?;
